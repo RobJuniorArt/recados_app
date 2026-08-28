34174ef92636b776e855e32d36699969bc66aa5b
Robson Junior <robson.fsj@gmail.com>
2026-08-28 09:34:04 -0300

    update insert whitelist and forbidNonWhitelisted for validadtion

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //remove chaves que nao estao no DTO
+      forbidNonWhitelisted: true, //levantar erro quando a chave nao existir
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

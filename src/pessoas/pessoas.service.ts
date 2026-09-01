49a954bb5e32ed2b5d9e0ac52c43004724a3664a
Robson Junior <robson.fsj@gmail.com>
2026-09-01 12:49:46 -0300

    update creating a create property with email unique on table

--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pessoa } from './entities/pessoa.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PessoasService {
-  create(createPessoaDto: CreatePessoaDto) {
-    return 'This action adds a new pessoa';
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+  ) {}
+
+  async create(createPessoaDto: CreatePessoaDto) {
+    try {
+      const dadosPessoa = {
+        name: createPessoaDto.name,
+        passwordHash: createPessoaDto.password,
+        email: createPessoaDto.email,
+      };
+
+      const novaPessoa = this.pessoaRepository.create(dadosPessoa);
+      await this.pessoaRepository.save(novaPessoa);
+      return novaPessoa;
+    } catch (e: any) {
+      if (e.code === '23505') {
+        throw new ConflictException('E-mail já está cadastrado.');
+      }
+      throw e;
+    }
   }
 
   findAll() {

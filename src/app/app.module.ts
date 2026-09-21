b57e359dcb58a30733fb35ac368855e6402196d4
Robson Junior <robson.fsj@gmail.com>
2026-09-21 20:13:44 -0300

    update testing create partial configs with namespaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RecadosModule } from '../recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import appConfig from './app.config';
+import { RecadosModule } from 'src/recados/recados.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      // envFilePath: ['env/.env'],
-      // ignoreEnvFile: false,
-      // validationSchema: Joi.object({
-      //   //DB_TYPE: Joi.required(),
-      // }) as any,
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [appConfig],
-    }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASS,
-      autoLoadEntities: Boolean(process.env.DB_AUTOLOADENTITIES), //carrega entidades sem precisar especificalas
-      synchronize: Boolean(process.env.DB_SYNCHRONIZE), //sincorniza tudo aqui com o bg (não usar em PROD)
-    }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [],
-    //   inject: [],
-    //   useFactory: async () => (configService: ConfigService) => {
-    //     return {
-    //       type: configService.get<'postgres'>('database.type'),
-    //       host: configService.get<string>('database.host'),
-    //       port: configService.get<number>('database.port'),
-    //       username: configService.get<string>('database.username'),
-    //       database: configService.get<string>('database.database'),
-    //       password: configService.get<string>('database.password'),
-    //       autoLoadEntities: configService.get<boolean>(
-    //         'database.autoLoadEntities',
-    //       ),
-    //       synchronize: configService.get<boolean>('database.synchonize'),
-    //     };
-    //   },
+    ConfigModule.forRoot(),
+    ConfigModule.forFeature(appConfig),
+    //TypeOrmModule.forRoot({,
+    //   type: process.env.DB_TYPE as 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: Number(process.env.DB_PORT),
+    //   username: process.env.DB_USER,
+    //   database: process.env.DB_NAME,
+    //   password: process.env.DB_PASS,
+    //   autoLoadEntities: Boolean(process.env.DB_AUTOLOADENTITIES), //carrega entidades sem precisar especificalas
+    //   synchronize: Boolean(process.env.DB_SYNCHRONIZE), //sincorniza tudo aqui com o bg (não usar em PROD)
     // }),
-    // RecadosModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(appConfig)],
+      inject: [appConfig.KEY],
+      useFactory: async (appConfiguration: ConfigType<typeof appConfig>) => {
+        return {
+          type: appConfiguration.database.type,
+          host: appConfiguration.database.host,
+          port: appConfiguration.database.port,
+          username: appConfiguration.database.username,
+          database: appConfiguration.database.database,
+          password: appConfiguration.database.password,
+          autoLoadEntities: appConfiguration.database.autoLoadEntities,
+          synchronize: appConfiguration.database.synchronize,
+        };
+      },
+    }),
+    RecadosModule,
     PessoasModule,
   ],
   controllers: [AppController],

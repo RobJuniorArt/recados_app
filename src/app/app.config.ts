7f14f85d739b9aecc2be09dfd56a1d66ec36ebf4
Robson Junior <robson.fsj@gmail.com>
2026-09-21 19:40:36 -0300

    update using config service

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  database: {
+    type: process.env.DB_TYPE as 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    autoLoadEntities: Boolean(process.env.DB_AUTOLOADENTITIES),
+    synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+  },
+  environment: process.env.NODE_ENV || 'development',
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from '../recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
-import * as Joi from '@hapi/joi';
+import appConfig from './app.config';
 
 @Module({
   imports: [
@@ -15,10 +15,10 @@ import * as Joi from '@hapi/joi';
       // validationSchema: Joi.object({
       //   //DB_TYPE: Joi.required(),
       // }) as any,
-      
     }),
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [appConfig],
     }),
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as 'postgres',
@@ -30,7 +30,25 @@ import * as Joi from '@hapi/joi';
       autoLoadEntities: Boolean(process.env.DB_AUTOLOADENTITIES), //carrega entidades sem precisar especificalas
       synchronize: Boolean(process.env.DB_SYNCHRONIZE), //sincorniza tudo aqui com o bg (não usar em PROD)
     }),
-    RecadosModule,
+    // TypeOrmModule.forRootAsync({
+    //   imports: [],
+    //   inject: [],
+    //   useFactory: async () => (configService: ConfigService) => {
+    //     return {
+    //       type: configService.get<'postgres'>('database.type'),
+    //       host: configService.get<string>('database.host'),
+    //       port: configService.get<number>('database.port'),
+    //       username: configService.get<string>('database.username'),
+    //       database: configService.get<string>('database.database'),
+    //       password: configService.get<string>('database.password'),
+    //       autoLoadEntities: configService.get<boolean>(
+    //         'database.autoLoadEntities',
+    //       ),
+    //       synchronize: configService.get<boolean>('database.synchonize'),
+    //     };
+    //   },
+    // }),
+    // RecadosModule,
     PessoasModule,
   ],
   controllers: [AppController],

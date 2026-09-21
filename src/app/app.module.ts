84e73e67bef000e9b053db44a24e808c9a791e21
Robson Junior <robson.fsj@gmail.com>
2026-09-21 14:19:53 -0300

    update improving ambient variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,19 @@ import { PessoasModule } from 'src/pessoas/pessoas.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: process.env.DB_TYPE as 'postgres',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       database: process.env.DB_NAME,
       password: process.env.DB_PASS,
-      autoLoadEntities: true, //carrega entidades sem precisar especificalas
-      synchronize: true, //sincorniza tudo aqui com o bg (não usar em PROD)
+      autoLoadEntities: Boolean(process.env.DB_AUTOLOADENTITIES), //carrega entidades sem precisar especificalas
+      synchronize: Boolean(process.env.DB_SYNCHRONIZE), //sincorniza tudo aqui com o bg (não usar em PROD)
     }),
     RecadosModule,
     PessoasModule,
